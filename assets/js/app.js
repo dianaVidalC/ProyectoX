1890e2a3474b0437816d2f2e469ad97b9b705ee2
Erika Vidal <diann852@gmail.com>
2017-04-25 11:46:28 -0500

    creando transicion

---
+++ assets/js/app.js
@@ -0,0 +1,8 @@
+var transicionHospedaje=document.getElementById("transicion-hospedaje");
+var transicionHospedaje=document.getElementById("icon-school");
+var transicionHospedaje=document.getElementById("icon-scissors");
+var transicionHospedaje=document.getElementById("icon-cool2");
+
+function transicionOver(){
+
+}
